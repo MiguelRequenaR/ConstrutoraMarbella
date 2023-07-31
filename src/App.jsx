5c325fba176f8a3382333b1ddde8e548736b6f20
Miguel Angel <e_2019100844l@uncp.edu.pe>
2023-07-31 18:38:17 -0500

    primercambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import 'remixicon/fonts/remixicon.css';
+import './styles/style.css';
+import logo from './assets/images/logo.png';
+import ScrollReveal from 'scrollreveal';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    let menu = document.querySelector('.ri-menu-line');
+    let navlist = document.querySelector('.navlist');
+
+    menu.onclick = () => {
+      menu.classList.toggle('bx-menu');
+      navlist.classList.toggle('open');
+    };
+  }, []);
+
+  const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+  });
+  sr.reveal('.frontpage-text', {delay:200, origin:'top'});
 
   return (
     <>
-      <div>
-        <a rel="noreferrer" href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a rel="noreferrer" href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <a href="" className="logo">Marbella</a>
+
+        <ul className='navlist'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Clans</a></li>
+          <li><a href="#">Town hall</a></li>
+          <li><a href="#">Troops</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
+        <div className="bx bx-menu" ><i className="ri-menu-line"></i></div>
+
+      </header>
+
+      <section className='frontpage'>
+        <div className='frontpage-text'>
+            <h5>#2 Trending</h5>
+            <h4>Constructora</h4>
+            <h1>Constructora Marbella</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis accusamus provident magnam facilis laboriosam,
+              nemo voluptate dignissimos tempora ut. Reprehenderit voluptatum quisquam itaque quas odio sunt ex nulla, recusandae cumque!
+            </p>
+            <a href="">Compare</a>
+            <a href="" className='movie'><i className='ri-play-fill'></i>Mira un video</a>
+        </div>
+
+        <div className='logo-img'>
+          <img src={logo} alt="" />
+        </div>
+
+      </section>
+
+      <div className='icons'>
+        <a href="#"><i className='ri-instagram-line'></i></a>
+        <a href="#"><i className='ri-youtube-line'></i></a>
+        <a href="#"><i className='ri-facebook-line'></i></a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='scroll-down'>
+        <a href="#"><i className='ri-arrow-down-s-fill'></i></a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
