1a5efff9561e1b054d364a2fc408c0208b8bece6
Miguel Angel Requena Ramos <e_2019100844L@uncp.edu.pe>
2023-08-02 17:27:29 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Dashboard from './Pages/Dashboard'; 
 import 'remixicon/fonts/remixicon.css';
 import './styles/style.css';
 import logo from './assets/images/logo.png';
@@ -16,53 +18,58 @@ function App() {
 
 
   return (
-    <>
-      <header>
-        <a href="" className="logo">Marbella</a>
+    <BrowserRouter>
+      
+        <header>
+          <a href="" className="logo">Marbella</a>
 
-        <ul className='navlist'>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Nosotros</a></li>
-          <li><a href="#">Proyectos en Venta</a></li>
-          <li><a href="#">Proyectos Ejecutados</a></li>
-          <li><a href="#">Constructora</a></li>
-          <li><a href="#">Contacto</a></li>
-        </ul>
+          <ul className='navlist'>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Proyectos en Venta</a></li>
+            <li><a href="#">Proyectos Ejecutados</a></li>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
 
-        <div className="bx bx-menu" ><i className="ri-menu-line"></i></div>
+          <div className="bx bx-menu" ><i className="ri-menu-line"></i></div>
 
-      </header>
+        </header>
 
-      <section className='frontpage'>
-        <div className='frontpage-text'>
-            <h5>Número 1 en Proyectos de la Región</h5>
-            <h4>Grandes obras, Grandes personas</h4>
-            <h1>Constructora Marbella S.R.L.</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis accusamus provident magnam facilis laboriosam,
-              nemo voluptate dignissimos tempora ut. Reprehenderit voluptatum quisquam itaque quas odio sunt ex nulla, recusandae cumque!
-            </p>
-            <a href="">Contáctenos</a>
-            <a href="" className='movie'><i className='ri-play-fill'></i>Mira un video</a>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+
+        <section className='frontpage'>
+          <div className='frontpage-text'>
+              <h5>Número 1 en Proyectos de la Región</h5>
+              <h4>Grandes obras, Grandes personas</h4>
+              <h1>Constructora Marbella S.R.L.</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis accusamus provident magnam facilis laboriosam,
+                nemo voluptate dignissimos tempora ut. Reprehenderit voluptatum quisquam itaque quas odio sunt ex nulla, recusandae cumque!
+              </p>
+              <a href="">Contáctenos</a>
+              <a href="" className='movie'><i className='ri-play-fill'></i>Mira un video</a>
+          </div>
+
+          <div className='logo-img'>
+            <img src={logo} alt="" />
+          </div>
+
+        </section>
+
+        <div className='icons'>
+          <a href="#"><i className='ri-instagram-line'></i></a>
+          <a href="#"><i className='ri-youtube-line'></i></a>
+          <a href="#"><i className='ri-facebook-line'></i></a>
         </div>
 
-        <div className='logo-img'>
-          <img src={logo} alt="" />
+        <div className='scroll-down'>
+          <a href="#"><i className='ri-arrow-down-s-fill'></i></a>
         </div>
 
-      </section>
-
-      <div className='icons'>
-        <a href="#"><i className='ri-instagram-line'></i></a>
-        <a href="#"><i className='ri-youtube-line'></i></a>
-        <a href="#"><i className='ri-facebook-line'></i></a>
-      </div>
-
-      <div className='scroll-down'>
-        <a href="#"><i className='ri-arrow-down-s-fill'></i></a>
-      </div>
-
-    </>
+      </BrowserRouter>
   )
 }
 
